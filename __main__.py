9699381aaa4e0c1d215fd77a1de445e1061f05dc
Amy Wang <40009989+xiwang129@users.noreply.github.com>
2020-06-04 15:21:05 -0400

    Update __main__.py

--- __main__.py
+++ __main__.py
@@ -1,3 +1,60 @@
+import numpy as np
+import keras 
+from keras.datasets import mnist 
+from keras.models import Model 
+from keras.models import Sequential 
+from keras.utils import to_categorical
+from keras.layers import Input, Dense, Dropout, Activation, Flatten
+from keras.layers import Conv2D, MaxPooling2D
+from keras.applications.resnet50 import ResNet50
+
+(X_train, y_train), (X_test, y_test) = mnist.load_data()
+
+X_train = X_train.reshape(60000, 28, 28, 1)
+X_test = X_test.reshape(10000, 28, 28, 1)
+
+X_train = X_train.astype('float32')
+X_test = X_test.astype('float32')
+X_train /= 255
+X_test /= 255
+
+print('X_train shape:', X_train.shape)
+print(X_train.shape[0], 'train samples')
+print(X_test.shape[0], 'test samples')
+
+y_train = keras.utils.to_categorical(y_train, 10)
+y_test = keras.utils.to_categorical(y_test, 10)
+
+
+model = Sequential()
+model.add(Conv2D(32, kernel_size=(3, 3), activation='relu', input_shape=(28,28,1)))
+model.add(Conv2D(64, (3, 3), activation='relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Dropout(0.25))
+model.add(Flatten())
+model.add(Dense(128, activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(10, activation='softmax'))
+
+model.compile(loss=keras.losses.categorical_crossentropy,
+              optimizer=keras.optimizers.Adadelta(),
+              metrics=['accuracy'])
+
+model.summary()
+
+model.fit(X_train, y_train, batch_size=128, verbose=0, epochs=5, validation_data=(X_test, y_test))
+
+model.save('mnist_model.h5')
+score = model.evaluate(X_test, y_test, verbose=0)
+print('Test score:', score[0])
+print('Test accuracy:', score[1])
+
+image_index = 50
+plt.imshow(X_test[image_index].reshape(28, 28),cmap='Greys')
+pred = model.predict(X_test[image_index].reshape(1, 28, 28, 1))
+print(pred.argmax())
+
+
 def main():
     pass
 
