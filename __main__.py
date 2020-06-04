608b5816e832aea4348e3d2abb3c0498168a786b
Aanand Kainth <aanand@akainth.me>
2020-06-04 13:38:35 -0700

    Make functional out of box

--- __main__.py
+++ __main__.py
@@ -1,62 +1,66 @@
-import numpy as np
-import keras 
-from keras.datasets import mnist 
-from keras.models import Model 
-from keras.models import Sequential 
-from keras.utils import to_categorical
-from keras.layers import Input, Dense, Dropout, Activation, Flatten
+import asyncio
+
+import keras
+import websockets
+from keras.datasets import mnist
 from keras.layers import Conv2D, MaxPooling2D
-from keras.applications.resnet50 import ResNet50
+from keras.layers import Dense, Dropout, Flatten
+from keras.models import Sequential
 
-(X_train, y_train), (X_test, y_test) = mnist.load_data()
-
-X_train = X_train.reshape(60000, 28, 28, 1)
-X_test = X_test.reshape(10000, 28, 28, 1)
-
-X_train = X_train.astype('float32')
-X_test = X_test.astype('float32')
-X_train /= 255
-X_test /= 255
-
-print('X_train shape:', X_train.shape)
-print(X_train.shape[0], 'train samples')
-print(X_test.shape[0], 'test samples')
-
-y_train = keras.utils.to_categorical(y_train, 10)
-y_test = keras.utils.to_categorical(y_test, 10)
-
-
-model = Sequential()
-model.add(Conv2D(32, kernel_size=(3, 3), activation='relu', input_shape=(28,28,1)))
-model.add(Conv2D(64, (3, 3), activation='relu'))
-model.add(MaxPooling2D(pool_size=(2, 2)))
-model.add(Dropout(0.25))
-model.add(Flatten())
-model.add(Dense(128, activation='relu'))
-model.add(Dropout(0.5))
-model.add(Dense(10, activation='softmax'))
-
-model.compile(loss=keras.losses.categorical_crossentropy,
-              optimizer=keras.optimizers.Adadelta(),
-              metrics=['accuracy'])
-
-model.summary()
-
-model.fit(X_train, y_train, batch_size=128, verbose=0, epochs=5, validation_data=(X_test, y_test))
-
-model.save('mnist_model.h5')
-score = model.evaluate(X_test, y_test, verbose=0)
-print('Test score:', score[0])
-print('Test accuracy:', score[1])
-
-image_index = 50
-plt.imshow(X_test[image_index].reshape(28, 28),cmap='Greys')
-pred = model.predict(X_test[image_index].reshape(1, 28, 28, 1))
-print(pred.argmax())
+import matplotlib.pyplot as plt
 
 
 def main():
-    pass
+    (X_train, y_train), (X_test, y_test) = mnist.load_data()
+
+    X_train = X_train.reshape(60000, 28, 28, 1)
+    X_test = X_test.reshape(10000, 28, 28, 1)
+
+    X_train = X_train.astype('float32')
+    X_test = X_test.astype('float32')
+    X_train /= 255
+    X_test /= 255
+
+    print('X_train shape:', X_train.shape)
+    print(X_train.shape[0], 'train samples')
+    print(X_test.shape[0], 'test samples')
+
+    y_train = keras.utils.to_categorical(y_train, 10)
+    y_test = keras.utils.to_categorical(y_test, 10)
+
+    model = Sequential()
+    model.add(Conv2D(32, kernel_size=(3, 3), activation='relu', input_shape=(28, 28, 1)))
+    model.add(Conv2D(64, (3, 3), activation='relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Dropout(0.25))
+    model.add(Flatten())
+    model.add(Dense(128, activation='relu'))
+    model.add(Dropout(0.5))
+    model.add(Dense(10, activation='softmax'))
+
+    model.compile(loss=keras.losses.categorical_crossentropy,
+                  optimizer=keras.optimizers.Adadelta(),
+                  metrics=['accuracy'])
+
+    model.summary()
+
+    # model.fit(X_train, y_train, batch_size=128, verbose=0, epochs=5, validation_data=(X_test, y_test))
+    model.load_weights('mnist_model.h5')
+    model.save('mnist_model.h5')
+    score = model.evaluate(X_test, y_test, verbose=0)
+    print('Test score:', score[0])
+    print('Test accuracy:', score[1])
+
+    connected_dashboards = []
+
+    async def connect_dashboard(websocket, _):
+        connected_dashboards.append(websocket)
+
+    start_server = websockets.serve(connect_dashboard, "localhost", 5000)
+
+    asyncio.get_event_loop().run_until_complete(start_server)
+    print("Started a dashboard socket on port 5000")
+    asyncio.get_event_loop().run_forever()
 
 
 if __name__ == '__main__':
